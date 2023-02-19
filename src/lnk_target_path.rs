4c3418fa0867c9de43ef32aa893fe1772924948d
Jan Starke <jan.starke@t-systems.com>
2023-02-19 20:07:33 +0100

    first working version

--- src/lnk_target_path.rs
+++ src/lnk_target_path.rs
@@ -1,38 +1,71 @@
 use std::fmt::Display;
 
 use anyhow::bail;
-use lnk_parser::{LNKParser};
-
+use lnk_parser::LNKParser;
+use winparsingtools::structs::shell_items::Name;
 
 /// refer to <https://winprotocoldoc.blob.core.windows.net/productionwindowsarchives/MS-SHLLINK/%5bMS-SHLLINK%5d.pdf>
 pub struct LnkTargetPath(String);
+
+impl LnkTargetPath {
+    fn path_from_target_list(parser: &LNKParser) -> Option<Self> {
+        if let Some(id_list) = parser.get_link_target_id_list() {
+            let path = id_list
+                .items()
+                .filter(|si| si.class_type & 0x70 != 0x10)
+                .map(|shell_item| shell_item.name())
+                .filter(|s| !s.is_empty())
+                .collect::<Vec<String>>()
+                .join("\\")
+                .replace("\\\\", "\\");
+            Some(Self(path))
+        } else {
+            None
+        }
+    }
+}
 
 impl TryFrom<&LNKParser> for LnkTargetPath {
     type Error = anyhow::Error;
 
     fn try_from(parser: &LNKParser) -> Result<Self, Self::Error> {
+        // try to generate path from link_info structure
         if let Some(link_info) = parser.get_link_info() {
-            let common_path_suffix = match link_info.get_common_path_suffix() {
-                Some(s) => s,
-                None => bail!("missing common path suffix"),
-            };
+            assert_ne!(
+                link_info.flags.get_CommonNetworkRelativeLinkAndPathSuffix(),
+                link_info.flags.get_VolumeIDAndLocalBasePath()
+            );
 
             if *link_info.flags.get_VolumeIDAndLocalBasePath() {
                 let local_basepath = match link_info.get_local_base_path() {
                     None => bail!("missing local base path"),
                     Some(path) => path,
                 };
-                Ok(Self(
-                    format!("{local_basepath}\\{common_path_suffix}")
-                ))
+                match link_info.get_common_path_suffix() {
+                    None => Ok(Self(local_basepath.to_string())),
+                    Some(s) => Ok(Self(format!("{local_basepath}\\{s}"))),
+                }
             } else {
                 let relative_link = match link_info.get_common_network_relative_link() {
                     None => bail!("missing common network relative link"),
-                    Some(link) => link
+                    Some(link) => link,
                 };
-                Ok(Self(
-                    format!("{}\\{common_path_suffix}", relative_link.get_net_name().as_ref().unwrap())
-                ))
+
+                match link_info.get_common_path_suffix() {
+                    None => {
+                        if let Some(path) = Self::path_from_target_list(parser) {
+                            Ok(path)
+                        } else {
+                            Ok(Self(
+                                relative_link.get_net_name().as_ref().unwrap().to_string(),
+                            ))
+                        }
+                    }
+                    Some(s) => Ok(Self(format!(
+                        "{}\\{s}",
+                        relative_link.get_net_name().as_ref().unwrap()
+                    ))),
+                }
             }
         } else {
             bail!("missing link info")
